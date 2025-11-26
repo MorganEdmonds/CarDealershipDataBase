22b4806ab874c80519fa8f116142b9cc390ecef9
MorganEdmonds <morganedmonds@gmail.com>
2025-11-26 10:25:08 -0500

    Dealership insterts

---
+++ Dealership_Insterts.sql
@@ -0,0 +1,65 @@
+USE CarDealership;
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+
+VALUES( "Morgan's auto shop", "3334 Magic Lane", "1800-333-4444" );
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Sunset Motors', '1245 Ocean Boulevard', '1800-555-0101');
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Premier Auto Sales', '789 Highway Drive', '1800-555-0202');
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Victory Car Center', '456 Commerce Street', '1800-555-0303');
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Riverside Auto Group', '2100 River Road', '1800-555-0404');
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Elite Vehicle Exchange', '5678 Main Street', '1800-555-0505');
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Apex Auto Dealers', '901 Industrial Parkway', '1800-555-0606');
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Parkway Motors Inc', '3421 Parkway Avenue', '1800-555-0707');
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Diamond Auto Mall', '1567 Diamond District', '1800-555-0808');
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Metro Car Sales', '8900 Metro Circle', '1800-555-0909');
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Frontier Auto Works', '234 Frontier Lane', '1800-555-1010');
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Golden Gate Motors', '777 Bridge View', '1800-555-1111');
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Heritage Auto Group', '4455 Heritage Way', '1800-555-1212');
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Precision Auto Sales', '6789 Precision Court', '1800-555-1313');
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Skyline Vehicle Center', '9012 Skyline Drive', '1800-555-1414');
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Horizon Auto Depot', '1234 Horizon Street', '1800-555-1515');
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Liberty Motors LLC', '5543 Liberty Avenue', '1800-555-1616');
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Summit Auto Exchange', '7890 Summit Plaza', '1800-555-1717');
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Crown Car Company', '3210 Crown Point', '1800-555-1818');
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Velocity Auto Sales', '6677 Velocity Lane', '1800-555-1919');
+
+INSERT INTO Dealerships (Dealership_Name, Dealership_Address, Phone_Number)
+VALUES('Maple Street Motors', '4321 Maple Street', '1800-555-2020');
