5c48b107080e926986a210858fb93eb8a4bd64ce
MorganEdmonds <morganedmonds@gmail.com>
2025-11-26 13:35:50 -0500

    Inventory Inserts

---
+++ Inventory_Inserts.sql
@@ -0,0 +1,61 @@
+USE CarDealership;
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(1, 10065792456018597);
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(1, 10065792456018598);
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(2, 10065792456018599);
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(2, 10065792456018600);
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(3, 10065792456018601);
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(3, 10065792456018602);
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(4, 10065792456018603);
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(5, 10065792456018604);
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(5, 10065792456018605);
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(6, 10065792456018606);
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(7, 10065792456018607);
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(8, 10065792456018608);
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(8, 10065792456018609);
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(9, 10065792456018610);
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(10, 10065792456018611);
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(11, 10065792456018612);
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(12, 10065792456018613);
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(13, 10065792456018614);
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(14, 10065792456018615);
+
+INSERT INTO inventory (Dealership_id, Vin)
+VALUES(15, 10065792456018616);
