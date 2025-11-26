0ff1674965bd471607441bbb2b7a967af369d8c8
MorganEdmonds <morganedmonds@gmail.com>
2025-11-26 11:59:08 -0500

    vehicle inserts

---
+++ vehicle_inserts.sql
@@ -0,0 +1,63 @@
+USE CarDealership;
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold )
+
+VALUES(10065792456018597, 2004, "Ford", "explorer", "SUV", 100000, 10000.00, 0);
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold)
+VALUES(10065792456018598, 2015, "Toyota", "Camry", "Sedan", 45000, 18500.00, 0);
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold)
+VALUES(10065792456018599, 2018, "Honda", "CR-V", "SUV", 32000, 24000.00, 1);
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold)
+VALUES(10065792456018600, 2020, "Chevrolet", "Silverado", "Truck", 28000, 35000.00, 0);
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold)
+VALUES(10065792456018601, 2017, "Nissan", "Altima", "Sedan", 55000, 16500.00, 1);
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold)
+VALUES(10065792456018602, 2019, "Jeep", "Wrangler", "SUV", 40000, 32000.00, 0);
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold)
+VALUES(10065792456018603, 2016, "Hyundai", "Elantra", "Sedan", 62000, 12000.00, 1);
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold)
+VALUES(10065792456018604, 2021, "Ford", "F-150", "Truck", 15000, 42000.00, 0);
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold)
+VALUES(10065792456018605, 2014, "Mazda", "CX-5", "SUV", 78000, 14500.00, 1);
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold)
+VALUES(10065792456018606, 2019, "Subaru", "Outback", "SUV", 35000, 26500.00, 0);
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold)
+VALUES(10065792456018607, 2017, "Volkswagen", "Jetta", "Sedan", 48000, 15500.00, 1);
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold)
+VALUES(10065792456018608, 2020, "GMC", "Sierra", "Truck", 22000, 38000.00, 0);
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold)
+VALUES(10065792456018609, 2015, "Kia", "Sorento", "SUV", 70000, 13500.00, 1);
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold)
+VALUES(10065792456018610, 2018, "BMW", "3 Series", "Sedan", 38000, 28000.00, 0);
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold)
+VALUES(10065792456018611, 2016, "Dodge", "Ram 1500", "Truck", 65000, 25000.00, 1);
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold)
+VALUES(10065792456018612, 2019, "Lexus", "RX 350", "SUV", 30000, 38500.00, 0);
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold)
+VALUES(10065792456018613, 2017, "Audi", "A4", "Sedan", 52000, 22000.00, 1);
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold)
+VALUES(10065792456018614, 2021, "Tesla", "Model 3", "Sedan", 12000, 45000.00, 0);
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold)
+VALUES(10065792456018615, 2014, "Acura", "MDX", "SUV", 85000, 18000.00, 1);
+
+INSERT INTO Vehicles (Vin, Vehicle_Year, Make, Model, VehicleType, Odometer, Price, Sold)
+VALUES(10065792456018616, 2018, "Mercedes-Benz", "C-Class", "Sedan", 42000, 32000.00, 0);
+
